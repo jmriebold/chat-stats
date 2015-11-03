b2cf5ea8f4edf97007f9a96b2818978d5521917e
John Riebold <jmriebold@gmail.com>
2015-11-02 20:10:44 -0800

    Switched from double to single quotes, removed superfluous import.

--- chat-stats.py
+++ chat-stats.py
@@ -22,30 +22,29 @@ by day, by word. Finally, day_timeseries.txt contains number of words used
 per day.
 """
 
-from __future__ import division
-import sys
-import os
-import re
-from collections import defaultdict, Counter
 import datetime
+import os
 import random
+import re
+import sys
+from collections import defaultdict, Counter
 
 import numpy
 
 
 # Extract and return name of message sender from line
 def process_name(line):
-    name = re.sub("^.*\d{2}:\d{2}:\d{2}: ", "", line)
-    name = re.sub("^<(\w+\s*\w*\s*\w*)>.*", "\\1", name).strip()
+    name = re.sub('^.*\d{2}:\d{2}:\d{2}: ', '', line)
+    name = re.sub('^<(\w+\s*\w*\s*\w*)>.*', '\\1', name).strip()
 
     return name
 
 
 # Return set of chat participants
 def get_participants(transcript):
-    with open(transcript, 'r', encoding="utf8") as in_file:
+    with open(transcript, 'r', encoding='utf8') as in_file:
         names = list(set(process_name(line).lower() for line in
-                         in_file.readlines() if line.startswith("[hangouts.py]")))
+                         in_file.readlines() if line.startswith('[hangouts.py]')))
 
     return names
 
@@ -64,9 +63,9 @@ def initialize_table(nrows, row_names, ndays):
 
 # Find and return timestamp of message
 def get_datetime(line):
-    timestamp = re.sub(".*(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}).*", "\\1",
+    timestamp = re.sub('.*(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}).*', '\\1',
                        line).strip()
-    timestamp = datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+    timestamp = datetime.datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
 
     return timestamp
 
@@ -80,8 +79,8 @@ def calculate_days(start_date, end_date):
 
 # Calculate and return the time of day in 6ths of an hour for a timestamp
 def calculate_daytime(timestamp):
-    hours = datetime.datetime.strftime(timestamp, "%H")
-    minutes = datetime.datetime.strftime(timestamp, "%M")
+    hours = datetime.datetime.strftime(timestamp, '%H')
+    minutes = datetime.datetime.strftime(timestamp, '%M')
 
     return (int(hours) * 60 + int(minutes)) // 10
 
@@ -89,9 +88,9 @@ def calculate_daytime(timestamp):
 # Remove hyperlinks, separate words with hyphens, slashes, ellipses, split on
 # whitespace
 def process_message(message):
-    message = re.sub("http[^\n\s]*", "", message)
+    message = re.sub('http[^\n\s]*', '', message)
 
-    return re.sub("-+|/+|\.{2}", " ", message).split()
+    return re.sub('-+|/+|\.{2}', ' ', message).split()
 
 
 # Strip punctuation and whitespace, retain contracted forms, lemmatize highly
@@ -99,20 +98,20 @@ def process_message(message):
 def process_word(word):
     # Contracted forms to spare from strip()ing
     contracted_forms = [
-        "can't", "could've", "couldn't", "didn't", "doesn't", "don't",
-        "hadn't", "hasn't", "haven't", "he'd", "he'll", "here's", "he's",
-        "i'd", "i'll", "i'm", "i've", "isn't", "it'd", "it'll", "it's",
-        "let's", "she'd", "she'll", "she's", "that'd", "that'll", "that's",
-        "there's", "there'll", "they're", "this'd", "this'll", "wasn't",
-        "we'd", "we're", "we've", "what'd", "what'll", "what's", "won't",
-        "would've", "wouldn't", "you'd", "you'll", "you're", "you've"
+        'can\'t', 'could\'ve', 'couldn\'t', 'didn\'t', 'doesn\'t', 'don\'t',
+        'hadn\'t', 'hasn\'t', 'haven\'t', 'he\'d', 'he\'ll', 'here\'s', 'he\'s',
+        'i\'d', 'i\'ll', 'i\'m', 'i\'ve', 'isn\'t', 'it\'d', 'it\'ll', 'it\'s',
+        'let\'s', 'she\'d', 'she\'ll', 'she\'s', 'that\'d', 'that\'ll', 'that\'s',
+        'there\'s', 'there\'ll', 'they\'re', 'this\'d', 'this\'ll', 'wasn\'t',
+        'we\'d', 'we\'re', 'we\'ve', 'what\'d', 'what\'ll', 'what\'s', 'won\'t',
+        'would\'ve', 'wouldn\'t', 'you\'d', 'you\'ll', 'you\'re', 'you\'ve'
     ]
 
     word = word.strip()
 
     # Remove punctuation
     if word not in contracted_forms:
-        word = re.sub("\W*", "", word)
+        word = re.sub('\W*', '', word)
 
     return word
 
@@ -168,7 +167,7 @@ def generatetext(name):
 
     sentence = name.title() + ': ' + sentence.capitalize()
 
-    # Capitalize "I"
+    # Capitalize 'I'
     sentence = re.sub('i(?=\')', 'I', sentence)
     sentence = re.sub(' i ', ' I ', sentence)
 
@@ -177,29 +176,29 @@ def generatetext(name):
 
 # Words of interest, edit this list to get timeseries data for these words
 # (e.g. for plotting over time, etc.)
-keywords = ["a", "b", "c"]
+keywords = ['a', 'b', 'c']
 
 # Stop list to exclude function words, etc.
 stop_list = [
-    "a", "about", "after", "again", "all", "also", "am", "an", "and",
-    "another", "any", "are", "as", "at", "be", "because", "behind", "been",
-    "being", "but", "by", "came", "can", "can't", "come", "could",
-    "couldn't", "could've", "did", "didn't", "do", "does", "doesn't", "doing",
-    "don't", "else", "even", "few", "for", "from", "get", "getting", "gets",
-    "go", "goes", "going", "gonna", "good", "got", "had", "hadn't", "has",
-    "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's",
-    "her", "here", "hers", "him", "his", "how", "i", "i'd", "i'll", "if",
-    "i'm", "in", "inside", "is", "isn't", "it", "it'd", "it'll", "it's", "its",
-    "i've", "just", "know", "let's", "like", "me", "my", "naw", "no", "not",
-    "now", "oh", "of", "off", "ok", "okay", "on", "one", "or", "our", "out",
-    "outside", "really", "right", "she", "she'd", "she'll", "she's", "should",
-    "that", "that'd", "that'll", "that's", "the", "their", "them", "then",
-    "there", "they", "these", "those", "think", "this", "this'd", "this'll",
-    "they're", "so", "some", "though", "to", "up", "us", "very", "was", "we",
-    "well", "went", "we're", "were", "we've", "what", "what's", "what'd",
-    "what'll", "when", "which", "while", "who", "why", "will", "with",
-    "without", "would", "wouldn't", "would've", "yeah", "yes", "you", "you'd",
-    "you'll", "your", "you're", "you've"
+    'a', 'about', 'after', 'again', 'all', 'also', 'am', 'an', 'and',
+    'another', 'any', 'are', 'as', 'at', 'be', 'because', 'behind', 'been',
+    'being', 'but', 'by', 'came', 'can', 'can\'t', 'come', 'could',
+    'couldn\'t', 'could\'ve', 'did', 'didn\'t', 'do', 'does', 'doesn\'t', 'doing',
+    'don\'t', 'else', 'even', 'few', 'for', 'from', 'get', 'getting', 'gets',
+    'go', 'goes', 'going', 'gonna', 'good', 'got', 'had', 'hadn\'t', 'has',
+    'hasn\'t', 'have', 'haven\'t', 'having', 'he', 'he\'d', 'he\'ll', 'he\'s',
+    'her', 'here', 'hers', 'him', 'his', 'how', 'i', 'i\'d', 'i\'ll', 'if',
+    'i\'m', 'in', 'inside', 'is', 'isn\'t', 'it', 'it\'d', 'it\'ll', 'it\'s', 'its',
+    'i\'ve', 'just', 'know', 'let\'s', 'like', 'me', 'my', 'naw', 'no', 'not',
+    'now', 'oh', 'of', 'off', 'ok', 'okay', 'on', 'one', 'or', 'our', 'out',
+    'outside', 'really', 'right', 'she', 'she\'d', 'she\'ll', 'she\'s', 'should',
+    'that', 'that\'d', 'that\'ll', 'that\'s', 'the', 'their', 'them', 'then',
+    'there', 'they', 'these', 'those', 'think', 'this', 'this\'d', 'this\'ll',
+    'they\'re', 'so', 'some', 'though', 'to', 'up', 'us', 'very', 'was', 'we',
+    'well', 'went', 'we\'re', 'were', 'we\'ve', 'what', 'what\'s', 'what\'d',
+    'what\'ll', 'when', 'which', 'while', 'who', 'why', 'will', 'with',
+    'without', 'would', 'wouldn\'t', 'would\'ve', 'yeah', 'yes', 'you', 'you\'d',
+    'you\'ll', 'your', 'you\'re', 'you\'ve'
 ]
 
 # Counters for words/bigrams/etc.
@@ -215,13 +214,13 @@ overall_bigrams = Counter()
 try:
     transcript = sys.argv[1]
 except IndexError:
-    raise IOError("Transcript file does not exist.")
+    raise IOError('Transcript file does not exist.')
 
 # Store results dir
 try:
     results_dir = sys.argv[2]
 except IndexError:
-    raise IOError("Please enter a results directory.")
+    raise IOError('Please enter a results directory.')
 
 # Ensure dir ends with /
 if results_dir[-1] != '/':
@@ -235,7 +234,7 @@ if not os.path.exists(results_dir):
 names = get_participants(transcript)
 
 # Calculate daterange
-with open(transcript, 'r', encoding="utf8") as in_file:
+with open(transcript, 'r', encoding='utf8') as in_file:
     lines = list(in_file)
     first = lines[0]
     n = -1
@@ -259,27 +258,27 @@ speaker_timeseries = initialize_table(len(names), names, ndays)
 word_timeseries = initialize_table(len(keywords), keywords, ndays)
 day_timeseries = numpy.zeros((24 * 6, 1), dtype=object)
 
-previous_timestamp = ""
+previous_timestamp = ''
 
-with open(transcript, 'r', encoding="utf8") as in_file:
+with open(transcript, 'r', encoding='utf8') as in_file:
     for line in in_file.readlines():
         line = line.lower()
 
         # Use prepended script name to differentiate between single- and
         # multi-line messages
-        if line.startswith("[hangouts.py]"):
+        if line.startswith('[hangouts.py]'):
             # Get timestamp of message
             timestamp = get_datetime(line)
 
             # Update previous timestamp
-            if previous_timestamp == "":
+            if previous_timestamp == '':
                 previous_timestamp = timestamp
 
             # Get speaker name
             name = process_name(line)
 
             # Get list of words in message and remove formatting
-            message = process_message(re.sub(".*<.*> (.*)", "\\1", line))
+            message = process_message(re.sub('.*<.*> (.*)', '\\1', line))
 
         else:
             message = process_message(line)
@@ -335,64 +334,64 @@ overall_total = sum(overall_words.values())
 overall_bigram_total = sum(overall_bigrams.values())
 
 # Print summary stats
-with open(results_dir + "summary.txt", 'w', encoding="utf8") as out_file:
-    out_file.write("GENERAL\n-------\n")
-    out_file.write("total length: " + str(overall_total) + " words" + "\ntime to read: " + str(
-        round(overall_total / 250 / 60, 2)) + " hours\n\n")
+with open(results_dir + 'summary.txt', 'w', encoding='utf8') as out_file:
+    out_file.write('GENERAL\n-------\n')
+    out_file.write('total length: ' + str(overall_total) + ' words' + '\ntime to read: ' + str(
+        round(overall_total / 250 / 60, 2)) + ' hours\n\n')
 
-    out_file.write("WORDS\n-----\n")
+    out_file.write('WORDS\n-----\n')
     for speaker, total in speaker_totals.most_common():
-        out_file.write(speaker + "\t" + str(total) + "\t" + str(round(float(
-            total / overall_total * 100), 2)) + "%\n")
-    out_file.write("\n")
+        out_file.write(speaker + '\t' + str(total) + '\t' + str(round(float(
+            total / overall_total * 100), 2)) + '%\n')
+    out_file.write('\n')
 
-    out_file.write("LEXICAL DIVERSITY\n-----------------\n")
+    out_file.write('LEXICAL DIVERSITY\n-----------------\n')
     for speaker in names:
-        out_file.write(speaker + "\t" + str(round(float(len(
-            speaker_words[speaker].keys()) / speaker_totals[speaker]), 2)) + "\n")
+        out_file.write(speaker + '\t' + str(round(float(len(
+            speaker_words[speaker].keys()) / speaker_totals[speaker]), 2)) + '\n')
 
 # Save timeseries data
-numpy.savetxt(results_dir + "speaker_timeseries.txt", speaker_timeseries,
-              delimiter="\t", fmt="%s")
-numpy.savetxt(results_dir + "word_timeseries.txt", word_timeseries,
-              delimiter="\t", fmt="%s")
-numpy.savetxt(results_dir + "day_timeseries.txt", day_timeseries,
-              delimiter="\t", fmt="%s")
+numpy.savetxt(results_dir + 'speaker_timeseries.txt', speaker_timeseries,
+              delimiter='\t', fmt='%s')
+numpy.savetxt(results_dir + 'word_timeseries.txt', word_timeseries,
+              delimiter='\t', fmt='%s')
+numpy.savetxt(results_dir + 'day_timeseries.txt', day_timeseries,
+              delimiter='\t', fmt='%s')
 
 # Save words
-with open(results_dir + "words.txt", 'w', encoding="utf8") as out_file:
+with open(results_dir + 'words.txt', 'w', encoding='utf8') as out_file:
     for speaker in names:
         for word, count in speaker_words[speaker].most_common():
-            out_file.write("%s\t%s\n" % (speaker, word))
+            out_file.write('%s\t%s\n' % (speaker, word))
 
 # Save overall word frequencies
-with open(results_dir + "overall_word_frequencies.txt", 'w', encoding="utf8") as out_file:
+with open(results_dir + 'overall_word_frequencies.txt', 'w', encoding='utf8') as out_file:
     for word, count in overall_words.most_common():
-        if count > 1 and word not in stop_list and re.match("^\d+$", word) \
+        if count > 1 and word not in stop_list and re.match('^\d+$', word) \
                 is None:
-            out_file.write("%s\t%s\t%s\n" % (word, count, round(count /
+            out_file.write('%s\t%s\t%s\n' % (word, count, round(count /
                                                                 overall_total * 100, 3)))
 
 # Save speaker word frequencies
-with open(results_dir + "speaker_word_frequencies.txt", 'w', encoding="utf8") as out_file:
+with open(results_dir + 'speaker_word_frequencies.txt', 'w', encoding='utf8') as out_file:
     for speaker in names:
         for word, count in speaker_words[speaker].most_common():
-            if count > 1 and word not in stop_list and re.match("^\d+$", word) \
+            if count > 1 and word not in stop_list and re.match('^\d+$', word) \
                     is None:
-                out_file.write("%s\t%s\t%s\t%s\n" % (speaker, word, count,
+                out_file.write('%s\t%s\t%s\t%s\n' % (speaker, word, count,
                                                      round(count / speaker_totals[speaker] * 100, 3)))
 
 # Save overall bigram frequencies
-with open(results_dir + "overall_bigram_frequencies.txt", 'w', encoding="utf8") as out_file:
+with open(results_dir + 'overall_bigram_frequencies.txt', 'w', encoding='utf8') as out_file:
     for bigram, count in overall_bigrams.most_common():
         if count > 1:
-            out_file.write("%s\t%s\t%s\n" % (bigram, count, round(count /
+            out_file.write('%s\t%s\t%s\n' % (bigram, count, round(count /
                                                                   overall_bigram_total * 100, 3)))
 
 # Save speaker bigram frequencies
-with open(results_dir + "speaker_bigram_frequencies.txt", 'w', encoding="utf8") as out_file:
+with open(results_dir + 'speaker_bigram_frequencies.txt', 'w', encoding='utf8') as out_file:
     for speaker in names:
         for bigram, count in speaker_bigrams[speaker].most_common():
             if count > 1:
-                out_file.write("%s\t%s\t%s\t%s\n" % (speaker, bigram, count,
+                out_file.write('%s\t%s\t%s\t%s\n' % (speaker, bigram, count,
                                                      round(count / sum(speaker_bigrams[speaker].values()) * 100, 3)))
